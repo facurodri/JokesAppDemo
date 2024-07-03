e5530e1022fc985c6dd4e18adb30f2c87f3b0a6f
Facundo <facurodri91@gmail.com>
2024-07-03 15:54:13 -0400

    Create ShowSearchForm controller method

--- JokesAppDemo/Controllers/JokesController.cs
+++ JokesAppDemo/Controllers/JokesController.cs
@@ -24,6 +24,11 @@ namespace JokesAppDemo.Controllers
         {
             return View(await _context.Joke.ToListAsync());
         }
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
 
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
